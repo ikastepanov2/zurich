73cedc0d0b0d13b54d58b6c6f7ebd13723b32bd8
ikastepanov2 <stepa987@gmail.com>
2026-01-28 02:14:23 +0100

    fix for comment

--- utils/api.Auth.ts
+++ utils/api.Auth.ts
@@ -2,7 +2,7 @@ import { request, chromium, APIRequestContext } from '@playwright/test';
 import { credentials } from './testData';
 
 export async function getAuthenticatedApiContext(): Promise<APIRequestContext> {
-  // 1️⃣ Login via browser (UI)
+  // Login via browser (UI)
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
@@ -14,10 +14,10 @@ export async function getAuthenticatedApiContext(): Promise<APIRequestContext> {
 
   await page.waitForURL('**/dashboard/**');
 
-  // 2️⃣ Extract cookies
+  // Extract cookies
   const cookies = await context.cookies();
 
-  // 3️⃣ Create API context with those cookies
+  // Create API context with those cookies
   const apiContext = await request.newContext({
     baseURL: 'https://opensource-demo.orangehrmlive.com',
     extraHTTPHeaders: {
